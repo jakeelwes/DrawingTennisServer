8a664171acc3eb9b5711e2200b96d0bf10dd116b
jakeelwes <jake.elwes@blueyonder.co.uk>
2015-11-30 20:12:02 -0600

    mongoose

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ var express = require('express')
 var app = express()
 var body_parser = require('body-parser')
 
+var mongoose = require ("mongoose");
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/HelloMongoose';
+var theport = process.env.PORT || 5000;
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+
+
 app.use(body_parser.json())
 var canvasdata = []
 
