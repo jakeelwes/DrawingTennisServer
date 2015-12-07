aad58a1e4baea5707a37cbc40443bcc449a276b9
jakeelwes <jake.elwes@blueyonder.co.uk>
2015-12-07 01:28:12 -0600

    fDate feature

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ mongoose.connect(uristring, function (err, res) {
 
 var userSchema = new mongoose.Schema({
     date: String,
+    formattedDate: String,
     name: String,
     index: String,
     data: String
